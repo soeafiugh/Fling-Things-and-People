89270dff96d45271745bf620edd4279565390209
moon hub <raiao11301219@gmail.com>
2025-09-19 19:54:35 +0900

    Update moon.lua

--- moon.lua
+++ moon.lua
@@ -36,34 +36,5 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
-local Tab = Window:CreateTab("defense", 4483362458) -- Title, Image
-
-ocal Button = Tab:CreateButton({
-   Name = "Invincible",
-   Callback = function()
-   -- The function that takes place when the button is pressed
-   end,
-})
-
-local Tab = Window:CreateTab("combat", 4483362458)
-
-local Toggle = Tab:CreateToggle({
-   Name = "choice",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-   -- The function that takes place when the toggle is pressed
-   -- The variable (Value) is a boolean on whether the toggle is true or false
-   end,
-})
-
-Toggle:Set(false)
-
-ocal Button = Tab:CreateButton({
-   Name = "Reskill",
-   Callback = function()
-   -- The function that takes place when the button is pressed
-   end,
-})
 
 
